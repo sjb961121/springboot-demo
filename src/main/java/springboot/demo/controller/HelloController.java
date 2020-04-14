e41be350ff0d23798136a4286c095fd5a406717a
Sun <sjb961121@126.com>
2020-04-14 09:11:44 +0800

    bootstrap commit

---
+++ src/main/java/springboot/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package springboot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+//    @RequestMapping("/hello")
+//    public String hello(@RequestParam(name = "name") String name, Model model){
+//        model.addAttribute("name",name);
+//        return "index";
+//    }
+    @RequestMapping("/")
+    public String hello(){
+        return "index";
+    }
+
+}
